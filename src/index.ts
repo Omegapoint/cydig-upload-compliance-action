b3ed2d43556009d5ccb77ce0b789cda6a912f2fb
Linnéa Oxenwaldt <linnea.oxenwaldt@omegapoint.se>
2026-02-25 10:29:35 +0100

    trying with dynamic import

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { ComplianceStateService } from './lib/ComplianceStateService';
 import { getContentOfFile } from './lib/JsonService';
 import { CyDigConfig } from './lib/types/CyDigConfig';
-import * as core from '@actions/core';
-import * as github from '@actions/github';
 
 async function runUpdateComplianceStateTask(): Promise<void> {
+    let core: typeof import('@actions/core') | undefined;
     try {
+        core = await import('@actions/core');
+        const github = await import('@actions/github');
         const codeRepositoryName: string = github.context.repo.repo;
         const subscriptionId: string = core.getInput('subscriptionId');
         const cydigConfigPath: string = core.getInput('cydigConfigPath');
@@ -23,7 +24,11 @@ async function runUpdateComplianceStateTask(): Promise<void> {
         await complianceStateService.createAndSendComplianceState(teamName, codeRepositoryName, subscriptionId);
     } catch (error) {
         // Fail the workflow run if an error occurs
-        core.setFailed(error);
+        if (core) {
+            core.setFailed(error);
+        } else {
+            console.error(error);
+        }
     }
 }
 
